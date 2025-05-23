bbf8385d2c79460ff45e1c40d3319eb2b26f0bd0
sudarshan <sudarshanchakraborty420@gmail.com>
2025-05-24 00:55:52 +0530

    set Keyboard Interrupt handling flow

--- src/keyboard.c
+++ src/keyboard.c
@@ -2,22 +2,21 @@
 #include "include/screen.h"
 
 
-char scancode_to_ascii(unsigned char scancode) {
+static char scancode_to_ascii(unsigned char scancode) {
     static char map[128] = {
-        0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+        0, 27, '1','2','3','4','5','6','7','8','9','0','-','=','\b',
         '\t',
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+        'q','w','e','r','t','y','u','i','o','p','[',']','\n',
         0,
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+        'a','s','d','f','g','h','j','k','l',';','\'','`',
         0,
-        '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
+        '\\','z','x','c','v','b','n','m',',','.','/',0,
         '*',
         0,
         ' ',
         0,
     };
-    if (scancode > 127)
-        return 0;
+    if (scancode > 127) return 0;
     return map[scancode];
 }
 
